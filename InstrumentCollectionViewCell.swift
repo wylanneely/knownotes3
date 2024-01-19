06b9c5fb27073e88111bf6f92339fdcae432b887
Wylan Neely <wylanneely@gmail.com>
2024-01-18 17:43:32 -0800

    Created Collectionview cell class for Instruments

---
+++ InstrumentCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  InstrumentCollectionViewCell.swift
+//  KnowNotes3
+//
+//  Created by Wylan L Neely on 1/18/24.
+//
+
+import UIKit
+
+class InstrumentCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
