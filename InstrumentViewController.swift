f8024ef610e7122c7026fa4e76b3033007310bc2
Wylan Neely <wylanneely@gmail.com>
2024-01-26 23:11:07 -0800

    refactored instrumentViewController

--- InstrumentViewController.swift
+++ InstrumentViewController.swift
@@ -9,14 +9,15 @@ import UIKit
 
 class InstrumentViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    @IBOutlet weak var collection: UICollectionView!
-       
        override func viewDidLoad() {
            super.viewDidLoad()
 
-           // Do any additional setup after loading the view.
        }
-
+    
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var collection: UICollectionView!
     
     //MARK: - CollectionView
     
@@ -32,13 +33,7 @@ class InstrumentViewController: UIViewController, UICollectionViewDelegate, UICo
             return UICollectionViewCell()
     }
 
-   
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    
 
 }
