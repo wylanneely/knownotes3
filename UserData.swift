f69326a250e9cc6f2e751158b9dfd6dd588f019f
Wylan Neely <wylanneely@gmail.com>
2024-02-02 20:17:06 -0800

    created user data class

---
+++ UserData.swift
@@ -0,0 +1,19 @@
+//
+//  UserData.swift
+//  KnowNotes3
+//
+//  Created by Wylan L Neely on 2/2/24.
+//
+
+import Foundation
+
+
+class UserData {
+    
+    var isGrandPianoUnlocked: Bool = false
+    var isViolinUnlocked:Bool = false
+    var isElectricGuitarUnlocked: Bool = false
+    var isAccousticGuitarUnlocked: Bool = false
+    
+    
+}
