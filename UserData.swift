265524271e747441c4cc79dbca539d09a249b987
Wylan Neely <wylanneely@gmail.com>
2024-02-03 20:36:40 -0800

    added userData and userdataController
    instrument unlocks
    round unlocks
    highscores
    
    keys in the controller

--- UserData.swift
+++ UserData.swift
@@ -10,10 +10,52 @@ import Foundation
 
 class UserData {
     
-    var isGrandPianoUnlocked: Bool = false
+    //MARK: - Instruments Unlocked
+    
+    var isGrandPianoUnlocked: Bool = true
+    var isAccousticGuitarUnlocked: Bool = false
     var isViolinUnlocked:Bool = false
     var isElectricGuitarUnlocked: Bool = false
-    var isAccousticGuitarUnlocked: Bool = false
+    
+    //MARK: - High Scores
+    
+    var grandPianoHScore: Int = 0
+    var accousticGuitarHScore: Int = 0
+    var violinHScore: Int = 0
+    var electricGuitarHScore: Int = 0
+    
+    //MARK: - Rounds Unlocked
+    
+    //Grand Piano
+    var isGPRound1Unlocked: Bool = true
+    var isGPRound2Unlocked: Bool = false
+    var isGPRound3UNlocked: Bool = false
+    
+    //Accoustic Guitar // if instrument is unlocked, round1 is unlocked
+    var isAGRound2Unlocked: Bool = false
+    var isAGRound3Unlocked: Bool = false
+    //Violin
+    var isVRound2Unlocked: Bool = false
+    var isVRound3Unlocked: Bool = false
+    //Electric Guitar
+    var isEGRound2Unlocked: Bool = false
+    var isEGROund3Unlocked: Bool = false
+    
+}
+
+class UserController {
+    
+    
+    
+    
+    
+    //MARK: - UserDefaults Keys
+    //instruments
+    let accousticGuitarKey: String = "isAccousticGuitarUnlocked"
+    let violinKey: String = "isViolinUnlocked"
+    let electricGuitarKey: String = "isElectricGuitarKeyUnlocked"
+    //scores
+    let grandPianoHSKey: String = ""
     
     
 }
