27d5d241692b73864268bd2a99d3eb974da091c0
Wylan Neely <wylanneely@gmail.com>
2024-02-09 21:26:56 -0800

    Updated StartpageViewController
    added user data + controller
    sent userdata through segue to InstrumentViewController
    added code to dtartbuttontapped

--- StartPageViewController.swift
+++ StartPageViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class StartPageViewController: UIViewController {
+    
+    let UserDataController = UserController()
+    var userData: UserData?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //set the initial userdefault data keys and values, should only be ran one time upon opening app
+        UserDataController.setDefaultDataValues()
+    }
+    
+    @IBAction func startButtonTapped(_ sender: Any) {
+        if let savedUserData = UserDataController.getUserData() {
+                    self.userData = savedUserData
+                }
 
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? InstrumentViewController {
+            if let userData = self.userData {
+                vc.userData = userData
+            }
+        }
+    }
 
+    
+    
 }
